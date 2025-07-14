9fd776890c60fca3ee5a0b83e71a3bdf9c6d307b
manoj <manoj@gmail.com>
2025-07-15 01:18:25 +0530

    new file

---
+++ src/main/java/com/example/demo/SampleDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleDeployApplication.class, args);
+	}
+
+}
